f74107fb934e15a146fe1f7905142a2148534613
Ben Navetta <ben.navetta@gmail.com>
2018-07-26 00:32:38 +0100

    Initial setup

---
+++ kernel/src/main.rs
@@ -0,0 +1,29 @@
+#![feature(panic_implementation)]
+#![no_std]
+#![no_main]
+
+use bootloader_precompiled;
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
