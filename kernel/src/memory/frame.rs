131688ed2fba62229a7ddcc82b9b8656d9fc7210
Ben Navetta <ben.navetta@gmail.com>
2019-05-07 16:29:45 -0400

    Alloc/free seem to work?!

--- kernel/src/memory/frame.rs
+++ kernel/src/memory/frame.rs
@@ -23,25 +23,93 @@ use bootloader::BootInfo;
 use bootloader::bootinfo::{MemoryRegionType, FrameRange};
 use intrusive_collections::{intrusive_adapter, LinkedList, LinkedListLink};
 use log::info;
-use x86_64::{PhysAddr, VirtAddr};
-
-use kutil::bitmap::Bitmap;
+use spin::Mutex;
+use x86_64::VirtAddr;
 
 const MAX_ORDER: usize = 11;
 const FRAME_SIZE: usize = 4096;
 
 struct Region {
+    /// Link in the parent FrameAllocator
+    link: LinkedListLink,
+
     /// Number of 4KiB physical page frames
     num_frames: u64,
 
+    /// Inner, mutable state
+    inner: Mutex<RegionInner>,
+}
+
+/// Inner, mutable state for a region. This layering is necessary because intrusive collection
+/// members need to be immutable (see https://github.com/Amanieu/intrusive-rs/issues/19). To get
+/// around this, all the mutable bits go in an inner struct, which the outer one wraps in a Mutex.
+/// Then, we can pass around &'static Region pointers and keep all the state for a region in its
+/// first page.
+struct RegionInner {
     // Bitmap tree
     bitmaps: [&'static mut [u8]; MAX_ORDER + 1],
 
     // Free lists for each order
     free_lists: [LinkedList<FreeBlockAdapter>; MAX_ORDER + 1],
+}
 
-    // Link in the global region list
-    link: LinkedListLink,
+// TODO: for consistency, make this just deal with indices internally, except where needed (free list)
+
+impl RegionInner {
+    fn mark_allocated<T>(&mut self, region: &Region, block: &T, order: usize, allocated: bool) {
+        self.bitmaps[order].set_bit(region.block_index(block, order), allocated)
+    }
+
+    fn is_allocated<T>(&self, region: &Region, block: &T, order: usize) -> bool {
+        self.bitmaps[order].get_bit(region.block_index(block, order))
+    }
+
+    fn free(&mut self, region: &Region, block: *mut u8, order: usize) {
+        let index = region.block_index(unsafe { block.as_ref() }.unwrap(), order);
+        assert!(self.bitmaps[order].get_bit(index), "Freeing an already-freed block");
+
+        // From David's fancy bit manipulation
+        let sibling = (index) - 1 + (((index) & 1) << 1);
+
+        if self.bitmaps[order].get_bit(sibling) || order == MAX_ORDER {
+            let block = unsafe { FreeBlock::from_address(VirtAddr::from_ptr(block)) };
+            self.free_lists[order].push_back(block);
+            self.mark_allocated(region, block, order, false);
+        } else {
+            // Reunite the buddies!
+
+            let sibling_block: *const FreeBlock = region.block_addr::<FreeBlock>(sibling, order).as_ptr();
+            unsafe { self.free_lists[order].cursor_mut_from_ptr(sibling_block) }.remove();
+            self.bitmaps[order].set_bit(sibling, true);
+
+            let parent_idx = (index - 1) >> 1;
+            assert!(self.bitmaps[order + 1].get_bit(parent_idx), "Parent of allocated block should be allocated");
+
+            self.free(region, region.block_addr::<u8>(parent_idx, order + 1).as_mut_ptr(), order + 1);
+        }
+    }
+
+    fn alloc(&mut self, region: &Region, order: usize) -> Option<*mut u8> {
+        if let Some(block) = self.free_lists[order].pop_front() {
+            self.mark_allocated(region, block, order, true);
+
+            unsafe { Some(mem::transmute(block)) }
+        } else if order < MAX_ORDER {
+            // Split a parent block
+
+            let parent = self.alloc(region, order + 1);
+            if let Some(parent) = parent {
+                let buddy = unsafe { parent.offset((order_frames(order) * FRAME_SIZE) as isize) };
+                self.free(region, buddy, order);
+                self.mark_allocated(region, unsafe { parent.as_ref() }.unwrap(), order, true);
+                Some(parent)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
 }
 
 impl Region {
@@ -53,9 +121,13 @@ impl Region {
 
         let region: *mut Region = start.as_mut_ptr();
         let region: &'static mut Region = unsafe { region.as_mut().unwrap() };
+        region.link = LinkedListLink::new();
+        // TODO: Using mem::zeroed() is kinda hacky, probably better to fully initialize RegionInner before setting on the Region
+        region.inner = Mutex::new(unsafe { mem::zeroed() });
+
+        let mut inner = region.inner.lock();
 
         region.num_frames = num_frames;
-        region.link = LinkedListLink::new();
 
         // Bitmaps start in the second page of the region
         let bitmaps_start = start + FRAME_SIZE;
@@ -64,15 +136,15 @@ impl Region {
             let bitmap_size = 1 << (MAX_ORDER - order);
             let bitmap_addr: VirtAddr = bitmaps_start + (bitmap_size - 1usize);
 
-            region.bitmaps[order] = unsafe {
+            inner.bitmaps[order] = unsafe {
                 let start_ptr = bitmap_addr.as_mut_ptr();
                 ptr::write_bytes(start_ptr, 0xff, bitmap_size as usize); // Mark everything as allocated
                 slice::from_raw_parts_mut(start_ptr, bitmap_size as usize)
             };
 
-            region.free_lists[order] = LinkedList::new(FreeBlockAdapter::new());
+            inner.free_lists[order] = LinkedList::new(FreeBlockAdapter::new());
 
-            info!("Order-{} bitmap is {} bytes long", order, region.bitmaps[order].len());
+            info!("Order-{} bitmap is {} bytes long", order, inner.bitmaps[order].len());
         }
 
         let mut frame_start = 2;
@@ -83,10 +155,10 @@ impl Region {
             let nframes = order_frames(order);
             if nframes <= remaining as usize {
                 info!("Marking order-{} block starting at offset {} as free", order, frame_start);
-                region.bitmaps[order].set_bit(frame_start as usize >> order, false);
+                inner.bitmaps[order].set_bit(frame_start as usize >> order, false);
 
                 let block = unsafe { FreeBlock::from_address(start + (frame_start * FRAME_SIZE as u64)) };
-                region.free_lists[order].push_back(block);
+                inner.free_lists[order].push_back(block);
 
                 frame_start += nframes as u64;
             } else {
@@ -96,9 +168,32 @@ impl Region {
 
         region
     }
-}
 
-intrusive_adapter!(RegionAdapter = &'static Region : Region { link: LinkedListLink });
+    fn block_index<T>(&self, block: &T, order: usize) -> usize {
+        let region_addr = (self as *const Region) as usize;
+        let block_addr = (block as *const T) as usize;
+
+        assert!(block_addr < region_addr + self.num_frames as usize * FRAME_SIZE, "Block does not belong to region");
+
+        let frame_offset = (block_addr - region_addr) / FRAME_SIZE;
+        frame_offset / order_frames(order)
+    }
+
+    fn block_addr<T>(&self, index: usize, order: usize) -> VirtAddr {
+        let region_addr = (self as *const Region) as usize;
+        VirtAddr::new((region_addr + order_frames(order) * index) as u64)
+    }
+
+    fn alloc(&self, order: usize) -> Option<*mut u8> {
+        let mut inner = self.inner.lock();
+        inner.alloc(self, order)
+    }
+
+    fn free(&self, order: usize, block: *mut u8) {
+        let mut inner = self.inner.lock();
+        inner.free(self, block, order);
+    }
+}
 
 struct FreeBlock {
     link: LinkedListLink
@@ -119,6 +214,8 @@ intrusive_adapter!(FreeBlockAdapter = &'static FreeBlock : FreeBlock { link: Lin
 const fn order_frames(order: usize) -> usize {
     1 << order
 }
+
+intrusive_adapter!(RegionAdapter = &'static Region : Region { link: LinkedListLink });
 
 pub struct FrameAllocator {
     regions: LinkedList<RegionAdapter>,
@@ -166,9 +263,25 @@ pub fn init(boot: &BootInfo) -> FrameAllocator {
         let addr = VirtAddr::from_ptr(region as *const Region);
         info!("Region at {:?}", addr - boot.physical_memory_offset);
 
-        for order in 0..=MAX_ORDER {
-            let bitmap_addr = VirtAddr::from_ptr(region.bitmaps[order].as_ptr());
-            info!("    Order {} bitmap at {:?}", order, bitmap_addr - boot.physical_memory_offset);
+        {
+            let inner = region.inner.lock();
+
+            for order in 0..=MAX_ORDER {
+                let bitmap_addr = VirtAddr::from_ptr(inner.bitmaps[order].as_ptr());
+                info!("    Order {} bitmap at {:?}", order, bitmap_addr - boot.physical_memory_offset);
+            }
+        }
+
+        for i in 0..20 {
+            let test_block = region.alloc(2);
+            info!("Allocated block {:?}", test_block);
+
+            if i % 2 == 0 {
+                if let Some(test_block) = test_block {
+                    region.free(2, test_block);
+                    info!("Freed block");
+                }
+            }
         }
     }
 
